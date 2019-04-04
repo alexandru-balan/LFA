f58f62e2701bec9218d7e190ca56f48f2d9d07e0
Alexandru-Balan <balan.alexandru1997@gmail.com>
2019-04-04 17:50:57 +0300

    Started documenting and commenting the code.

--- FA_to_REX/src/aftorex/Automaton.java
+++ FA_to_REX/src/aftorex/Automaton.java
@@ -7,13 +7,20 @@ import java.util.*;
 
 public class Automaton {
     private String initialState;
-    private TreeSet Q = new TreeSet<String>(Comparator.naturalOrder());
-    private TreeSet E = new TreeSet<String>(Comparator.naturalOrder());
-    private TreeSet F = new TreeSet<String>(Comparator.naturalOrder());
-    private TreeSet nextStates = new TreeSet<String>(Comparator.naturalOrder());
-    private TreeSet lambdaStates = new TreeSet<String>(Comparator.naturalOrder());
+    private TreeSet Q = new TreeSet<String>(Comparator.naturalOrder()); //States set
+    private TreeSet E = new TreeSet<String>(Comparator.naturalOrder()); //Symbols set
+    private TreeSet F = new TreeSet<String>(Comparator.naturalOrder()); //Final states set
+    private TreeSet nextStates = new TreeSet<String>(Comparator.naturalOrder()); //Set that retains the next possible states after reading one symbol
+    private TreeSet lambdaStates = new TreeSet<String>(Comparator.naturalOrder()); //Set that retains the next possible states after reading one symbol
+                                                                                    //and passing though multiple lambda transitions
     private Transition[] Transitions;
 
+    /***
+     *
+     * @param path_to_file = path in filesystem where the automaton is defined
+     * @throws InputMismatchException because each automaton file MUST begin with an integer (the number of transitions)
+     * @throws FileNotFoundException because path may be wrong
+     */
     Automaton(String path_to_file) throws InputMismatchException, FileNotFoundException {
         File file = new File(path_to_file);
         Scanner scanner = new Scanner(file);
@@ -21,6 +28,9 @@ public class Automaton {
         int nbOfTransitions = scanner.nextInt();
         Transitions = new Transition[nbOfTransitions];
 
+        /*reminder should have been the new_line character left over from reading the nb. of transitions, but instead
+        * it is also the first string from the next line
+        * BUG -> FIX ME*/
         String reminder = scanner.next();
         System.out.println(reminder);
 
@@ -30,19 +40,20 @@ public class Automaton {
             F.add(reminder.replace("@",""));
         }
 
+        /*Because of the bug above, special treatment has to be given to the first line in the file*/
         boolean first = true;
 
-
+        /*This for loop reads the automaton from the file and places the states and symbols in their respective sets*/
         for(int i = 0; i < nbOfTransitions; ++i) {
             String line = scanner.nextLine();
             String[] parts = line.split(" ");
             Transition transition;
-            if(first) {
+            if(first) { //special treatment for first line only
                 transition = new Transition(reminder,parts[1],parts[2]);
                 first = false;
             }
             else {
-                transition = new Transition(parts[0],parts[1],parts[2]);
+                transition = new Transition(parts[0],parts[1],parts[2]); //default treatment
             }
             Transitions[i] = transition;
             Q.add(parts[0].replace("@",""));
@@ -57,6 +68,10 @@ public class Automaton {
         }
     }
 
+    /***
+     *
+     * @return = a string with all the info about the read automaton
+     */
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
@@ -88,6 +103,12 @@ public class Automaton {
         return builder.toString();
     }
 
+    /***
+     *
+     * @param symbol = a symbol from the word that has to be accepted
+     * @return = -> true if the automaton can change states by reading this symbol
+     *           -> false if there are no transitions from the current states to other states with this symbol
+     */
     private boolean delta(String symbol) {
         TreeSet<String> replacer = new TreeSet<>(Comparator.naturalOrder());
         Iterator<String> iterator = nextStates.iterator();
